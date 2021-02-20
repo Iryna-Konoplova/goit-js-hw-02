b92146824831b5a19f5c8877eff1cb5cc08421af
Iryna Konoplova <a.irina773@gmail.com>
2021-02-20 02:02:05 +0200

    Решает задачи 1-3

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку
+// (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+const findLongestWord = function(string) {
+  // твой код
+    const arrayWords = string.split(' ');
+    let longestWord = arrayWords[0];
+    for (let i = 0; i < arrayWords.length; i += 1) {
+        if (longestWord.length < arrayWords[i].length) {
+            longestWord = arrayWords[i];
+        }
+    }
+    return longestWord;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
